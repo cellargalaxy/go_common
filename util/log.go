825f093ae71a20736d79c0118b4b744c185dc052
cellargalaxy <cellargalaxy@gmail.com>
2021-08-25 00:02:21 +0800

    InitLog

--- util/log.go
+++ util/log.go
@@ -19,18 +19,17 @@ import (
 	"time"
 )
 
-const LogPath = "log/log.log"
-
 const LogIdKey = "logid"
 const CallerKey = "caller"
 
-func InitLog() {
+func InitLog(projectName string) {
+	filename := fmt.Sprintf("log/%s_%d/log.log", projectName, CreateLogId())
 	lumberJackLogger := &lumberjack.Logger{
-		Filename:   LogPath, //日志文件的位置
-		MaxSize:    10,      //在进行切割之前，日志文件的最大大小（以MB为单位）
-		MaxBackups: 100,     //保留旧文件的最大个数
-		MaxAge:     30,      //保留旧文件的最大天数
-		Compress:   false,   //是否压缩/归档旧文件
+		Filename:   filename, //日志文件的位置
+		MaxSize:    10,       //在进行切割之前，日志文件的最大大小（以MB为单位）
+		MaxBackups: 100,      //保留旧文件的最大个数
+		MaxAge:     30,       //保留旧文件的最大天数
+		Compress:   false,    //是否压缩/归档旧文件
 	}
 	multiWriter := io.MultiWriter(os.Stdout, lumberJackLogger)
 	logrus.SetOutput(multiWriter)
