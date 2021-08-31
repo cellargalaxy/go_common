b7b9410c20fb32d95647880ddcf1e716011b0b7d
lixinzhao <lixinzhao@bytedance.com>
2021-08-31 18:20:53 +0800

    InitLog

--- util/log.go
+++ util/log.go
@@ -23,10 +23,10 @@ const LogIdKey = "logid"
 const CallerKey = "caller"
 
 func InitLog(projectName string) {
-	filename := fmt.Sprintf("log/%s_%d/log.log", projectName, CreateLogId())
+	filename := fmt.Sprintf("log/%s.log", projectName)
 	lumberJackLogger := &lumberjack.Logger{
 		Filename:   filename, //日志文件的位置
-		MaxSize:    10,       //在进行切割之前，日志文件的最大大小（以MB为单位）
+		MaxSize:    1,        //在进行切割之前，日志文件的最大大小（以MB为单位）
 		MaxBackups: 100,      //保留旧文件的最大个数
 		MaxAge:     30,       //保留旧文件的最大天数
 		Compress:   false,    //是否压缩/归档旧文件
