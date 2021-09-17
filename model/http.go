9534f2c8b0c275cda432e781c70c9bde51824de9
cellargalaxy <cellargalaxy@gmail.com>
2021-09-18 00:07:13 +0800

    备份

--- model/http.go
+++ model/http.go
@@ -1,6 +1,7 @@
 package model
 
 import (
+	"github.com/cellargalaxy/go_common/util"
 	"github.com/gin-gonic/gin"
 	"github.com/golang-jwt/jwt"
 )
@@ -9,6 +10,10 @@ type Claims struct {
 	jwt.StandardClaims
 	AllowReRequest bool   `json:"allow_re_request,omitempty"`
 	RequestId      string `json:"request_id,omitempty"`
+}
+
+func (this Claims) String() string {
+	return util.ToJsonString(this)
 }
 
 type HttpValidateInter interface {
@@ -20,4 +25,8 @@ type HttpRequestParam struct {
 	Url    string            `json:"url"`
 	Header map[string]string `json:"header"`
 	Body   string            `json:"body"`
+}
+
+func (this HttpRequestParam) String() string {
+	return util.ToJsonString(this)
 }
