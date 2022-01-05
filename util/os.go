5b0e28dbd3ec0759997ea1be86ade50daf832cd0
lixinzhao <lixinzhao@bytedance.com>
2022-01-05 14:24:09 +0800

    备份

--- util/os.go
+++ util/os.go
@@ -4,12 +4,22 @@ import (
 	"context"
 	"os"
 	"os/signal"
+	"runtime"
 	"strconv"
 	"strings"
 	"syscall"
 )
 
 const serverNameEnvKey = "server_name"
+
+func Defer(callback func(err interface{}, stack string)) {
+	if err := recover(); err != nil {
+		var buf [1024 * 4]byte
+		n := runtime.Stack(buf[:], false)
+		stack := string(buf[:n])
+		callback(err, stack)
+	}
+}
 
 func GetServerNameWithPanic() string {
 	value := GetServerName()
