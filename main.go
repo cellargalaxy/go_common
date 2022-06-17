4b49223275720f4f9f475d826ac716bbf585e683
lixinzhao <lixinzhao@bytedance.com>
2022-06-17 21:38:06 +0800

    PingPath

--- main.go
+++ main.go
@@ -1,14 +1,12 @@
 package main
 
 import (
-	"github.com/cellargalaxy/go_common/consd"
 	"github.com/cellargalaxy/go_common/model"
 	"github.com/cellargalaxy/go_common/util"
 	"time"
 )
 
 func init() {
-	consd.Init()
 	model.Init()
 	util.Init()
 }
