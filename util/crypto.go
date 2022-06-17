fbf533bd31bb5fb44532be3ba7392253bf161d03
lixinzhao <lixinzhao@bytedance.com>
2022-06-17 20:37:48 +0800

    GenAuthorizationJWT

--- util/crypto.go
+++ util/crypto.go
@@ -13,6 +13,10 @@ import (
 	"time"
 )
 
+func GenAuthorizationJWT(ctx context.Context, expire time.Duration, secret string) (string, string) {
+	token, _ := GenDefaultJWT(ctx, expire, secret)
+	return TokenKey, fmt.Sprintf("%s %s", BearerKey, token)
+}
 func GenDefaultJWT(ctx context.Context, expire time.Duration, secret string) (string, error) {
 	now := time.Now()
 	var claims model.Claims
