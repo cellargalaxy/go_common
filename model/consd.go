ab101cc177685b964be08cee1f85c2ad57c93d03
cellargalaxy <cellargalaxy@gmail.com>
2022-08-20 17:18:34 +0800

    Float64

--- model/consd.go
+++ model/consd.go
@@ -6,23 +6,52 @@ const (
 	HttpReRequestCode  = 3
 	HttpIllegalUriCode = 4
 )
-
-type Bool int
-
-func (this Bool) Bool() bool {
-	return this == BoolYes
-}
-
 const (
 	BoolYes Bool = 1
 	BoolNo  Bool = 2
 )
+
+type Bool int
+
+func (this Bool) Value() bool {
+	return this == BoolYes
+}
 
 func NewBool(value bool) Bool {
 	if value {
 		return BoolYes
 	}
 	return BoolNo
+}
+
+type Int int
+
+func (this Int) Value() int {
+	return int(this)
+}
+
+func NewInt(value int) Int {
+	return Int(value)
+}
+
+type Int64 int64
+
+func (this Int64) Value() int64 {
+	return int64(this)
+}
+
+func NewInt64(value int64) Int64 {
+	return Int64(value)
+}
+
+type Float64 float64
+
+func (this Float64) Value() float64 {
+	return float64(this)
+}
+
+func NewFloat64(value float64) Float64 {
+	return Float64(value)
 }
 
 const (
