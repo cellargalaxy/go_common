c363a2406ef0323dbeaf9ce67169fca745e38872
lixinzhao <lixinzhao@bytedance.com>
2022-08-31 22:22:06 +0800

    1

--- util/sync.go
+++ util/sync.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"github.com/panjf2000/ants/v2"
 	"github.com/sirupsen/logrus"
+	"sync"
 	"time"
 )
 
@@ -24,6 +25,79 @@ func ReleasePool(pools ...*ants.Pool) {
 		}
 		pools[i].Release()
 	}
+}
+
+type SingleGoPool struct {
+	poolName string
+	pool     *ants.Pool
+
+	lock     sync.Mutex
+	taskName string
+	cancel   func()
+}
+
+func (this *SingleGoPool) AddTask(ctx context.Context, name string, sleep time.Duration, task func(ctx context.Context, pool *SingleGoPool)) error {
+	this.lock.Lock()
+	defer this.lock.Unlock()
+
+	if this.taskName == name {
+		logrus.WithContext(ctx).WithFields(logrus.Fields{"name": name}).Info("单协程池，任务已添加")
+		return nil
+	}
+
+	this.Cancel(ctx)
+	this.taskName = name
+
+	return nil
+}
+func (this *SingleGoPool) addTask(ctx context.Context, sleep time.Duration, task func(ctx context.Context, pool *SingleGoPool)) error {
+	ctx, this.cancel = context.WithCancel(ctx)
+
+	var submit func()
+	submit = func() {
+		defer func() {
+			logrus.WithContext(ctx).WithFields(logrus.Fields{"name": this.GetName(ctx)}).Info("单协程池，退出")
+			go func() {
+				Sleep(ctx, sleep)
+				if CtxDone(ctx) {
+					logrus.WithContext(ctx).WithFields(logrus.Fields{"name": this.GetName(ctx)}).Info("单协程池，结束")
+					return
+				}
+				if this.pool.IsClosed() {
+					logrus.WithContext(ctx).WithFields(logrus.Fields{"name": this.GetName(ctx)}).Info("单协程池，关闭")
+					return
+				}
+
+				err := this.pool.Submit(submit)
+				if err != nil {
+					logrus.WithContext(ctx).WithFields(logrus.Fields{"name": this.GetName(ctx), "err": err}).Error("单协程池，添加任务异常")
+				}
+			}()
+		}()
+
+		task(ctx, this)
+	}
+
+	err := this.pool.Submit(submit)
+	if err != nil {
+		logrus.WithContext(ctx).WithFields(logrus.Fields{"name": this.GetName(ctx), "err": err}).Error("单协程池，添加任务异常")
+		this.Cancel(ctx)
+		return fmt.Errorf("单协程池，添加任务异常: %+v", err)
+	}
+
+	return nil
+}
+func (this *SingleGoPool) Cancel(ctx context.Context) {
+	logrus.WithContext(ctx).WithFields(logrus.Fields{"name": this.GetName(ctx)}).Info("单协程池，取消任务")
+	Cancel(this.cancel)
+}
+func (this *SingleGoPool) Release(ctx context.Context) {
+	logrus.WithContext(ctx).WithFields(logrus.Fields{"name": this.GetName(ctx)}).Info("单协程池，释放协程池")
+	this.Cancel(ctx)
+	ReleasePool(this.pool)
+}
+func (this *SingleGoPool) GetName(ctx context.Context) string {
+	return fmt.Sprintf("%s_%s", this.poolName, this.taskName)
 }
 
 func NewForeverSingleGoPool(ctx context.Context, name string, sleep time.Duration, task func(ctx context.Context, cancel func())) (*ants.Pool, error) {
