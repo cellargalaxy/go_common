7b2d7c2d25e058759d75f60e1580d067066cd776
cellargalaxy <cellargalaxy@gmail.com>
2026-09-10 16:30:21 +0800

    1

--- util/sync.go
+++ util/sync.go
@@ -88,11 +88,11 @@ func (this *SingleGoPool) AddDaemonTask(ctx context.Context, name string, sleep 
 }
 func (this *SingleGoPool) addDaemonTask(ctx context.Context, name string, sleep time.Duration, task func(cancelCtx context.Context, pool *SingleGoPool)) error {
 	submit := func() {
-		defer Defer(func(err interface{}, stack string) {
-			if err == nil {
+		defer Defer(func(panic any, stack string) {
+			if panic == nil {
 				logrus.WithContext(ctx).WithFields(logrus.Fields{"name": this.getName()}).Info("单协程池，退出")
 			} else {
-				logrus.WithContext(ctx).WithFields(logrus.Fields{"name": this.getName(), "err": err, "stack": stack}).Error("单协程池，退出")
+				logrus.WithContext(ctx).WithFields(logrus.Fields{"name": this.getName(), "panic": panic, "stack": stack}).Error("单协程池，退出")
 			}
 
 			go func() {
